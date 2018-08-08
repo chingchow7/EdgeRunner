4aa36ba3e8402b403408631615e98d7b8a99253d
Ching Chow <cchow7@uh.edu>
2018-08-08 17:02:37 -0500

    basic scene

---
+++ EdgeRunner/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = .25f;
+
+    private float rotationLerp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a")) {
+            rotationLerp = Mathf.Lerp(rotationLerp, -15f, .1f);
+            this.gameObject.transform.Translate(Vector3.back * speed);
+            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(rotationLerp, 0f, 90f));
+            return;
+        }
+
+        if (Input.GetKey("d")) {
+            rotationLerp = Mathf.Lerp(rotationLerp, 15f, .1f);
+            this.gameObject.transform.Translate(Vector3.forward * speed);
+            this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(rotationLerp, 0f, 90f));
+            return;
+        }
+        rotationLerp = Mathf.Lerp(rotationLerp, 0, .1f);
+        this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(rotationLerp, 0f, 90f));
+    }
+}
---
+++ EdgeRunner/Assets/Scripts/Secrets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rigidbody_;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody_ = this.GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody_.velocity = new Vector3(-10f, 0, 0);
+
+    }
+}
